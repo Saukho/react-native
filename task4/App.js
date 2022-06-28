6dfb665b37bfe38fcb133665fc12600cc19cdbd9
sami <sami.sihvonen1984@gmail.com>
2022-06-28 13:48:17 +0300

    tasks complete

--- task4/App.js
+++ task4/App.js
@@ -1,139 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  TextInput,
-  Button,
-  View,
-  Text,
-  Modal,
-  TouchableOpacity,
-  FlatList,
-  ScrollView,
-} from 'react-native';
-
+import {StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableOpacity} from 'react-native';
+import AddBoot from './components/AddBoot';
+import UpdateBoot from './components/UpdateBoot';
 const App = () => {
-  const [type, setType] = useState();
-  const [size, setSize] = useState();
-  const [list, addBoot] = useState([]);
-  const [updateId, setUpdateId] = useState(-1);
-  const [modalVisible, setModalVisible] = useState(false);
+  const [bootList, addBootList] = useState([]);
+  const [modalVisible, setModalVisible]=useState(false);
+  const [updateModalVisible, setUpdateModalVisible]=useState(false);
+  const [bootToUpdate, setBootToUpdate]=useState();
+  const [bootToUpdateIndex, setBootToUpdateIndex]=useState();
 
-  const bootTypeInputHandler = enteredText => {
-    setType(enteredText);
-  };
-  const bootSizeInputHandler = enteredSize => {
-    setSize(enteredSize);
-  };
-
-  const bootDataHandler = () => {
-    if (updateId != -1) {
-      list[updateId].type = type;
-      list[updateId].size = size;
-      addBoot(list);
-      setUpdateId(-1);
-    } else {
-      addBoot(list => [...list, {type: type, size: size}]);
-    }
+  const bootDataHandler = (id, type) => {
+    addBootList(bootList => [...bootList, {id:id,type:type}]);
     setModalVisible(false);
-    setSize('');
-    setType('');
   };
-
-  const updateItem = index => {
-    setUpdateId(index);
-    setType(list[index].type);
-    setSize(list[index].size);
-    openModal();
+  const bootUpdateHandler = (id, type) => {
+    bootList[bootToUpdateIndex]={id:id, type:type};
+    addBootList(bootList);
+    setUpdateModalVisible(false);
   };
-
-  const cancelBoot = () => {
+  const deleteBoot=(removeId)=>{
+    addBootList(bootList=>bootList.filter((boot, index)=>index!=removeId));
+  };
+  const showInputModal=()=>{
     setModalVisible(true);
-    setSize('');
-    setType('');
-  };
-  const keyHandler = (item, index) => {
-    return index.toString();
-  };
-
-  const openModal = () => {
-    setModalVisible(true);
-  };
-  function deleteBoot(removeId) {
-    addBoot(listOfBoots =>
-      listOfBoots.filter((boot, index) => index != removeId),
-    );
   }
-
-  const renderBoot = ({item, index}) => {
-    return (
-      <ScrollView style={styles.scrollviewstyle}>
-        <TouchableOpacity
-          key={index}
-          onPress={() => updateItem(index)}
-          onLongPress={() => deleteBoot(index)}>
-          <View style={styles.listitemstyle}>
-            <Text>
-              {item.type}: {item.size}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </ScrollView>
-    );
-  };
+  const hideInputModal=()=>{
+    setModalVisible(false);
+  }
+  const hideUpdateModal=()=>{
+    setUpdateModalVisible(false);
+  }
+  const updateBoot=(index)=>{
+    setBootToUpdateIndex(index);
+    setBootToUpdate(bootList[index]);
+    setUpdateModalVisible(true);
+  }
   return (
     <View style={styles.container}>
-      <Modal visible={modalVisible} animationType="slide">
-        <View style={styles.formStyle}>
-          <TextInput
-            style={styles.inputStyle}
-            placeholder="boot size"
-            onChangeText={bootSizeInputHandler}
-            value={size}
-          />
-          <TextInput
-            style={styles.inputStyle}
-            placeholder="boot type"
-            onChangeText={bootTypeInputHandler}
-            value={type}
-          />
-          <View style={styles.buttonView}>
-            <Button
-              style={styles.buttonStyle}
-              title="Ok"
-              onPress={bootDataHandler}
-            />
+      <AddBoot visibility={modalVisible} bootDataHandler={bootDataHandler} hideInputModal={hideInputModal}/>
+      <UpdateBoot visibility={updateModalVisible} bootUpdateHandler={bootUpdateHandler} bootToUpdate={bootToUpdate} hideUpdateModal={hideUpdateModal}/>
+      <Button onPress={showInputModal} title="Add boot" />
+      <Text>Boot list</Text>
+      <ScrollView style={styles.scrollviewstyle}>
+        {bootList.map((item, index) => (
+          <TouchableOpacity key={index} onLongPress={()=>deleteBoot(index)} onPress={()=>updateBoot(index)}>
+          <View style={styles.listitemstyle}>
+            <Text>
+              {item.id}: {item.type}
+            </Text>
           </View>
-          <View style={styles.buttonView}>
-            <Button
-              style={styles.buttonStyle}
-              title="Cancel"
-              onPress={cancelBoot}
-            />
-          </View>
-        </View>
-      </Modal>
-      <View style={styles.listStyle}>
-        <Button
-          style={styles.buttonStyle}
-          title="Add new Boot"
-          onPress={() => openModal()}
-        />
-        <AddBoot />
-        <FlatList
-          style={styles.flatliststyle}
-          keyExtractor={keyHandler}
-          data={list}
-          renderItem={renderBoot}
-        />
-      </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -141,57 +59,33 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    width: '100%',
-  },
-  headerStyle: {
-    fontSize: 18,
-    marginVertical: 12,
-  },
-  formStyle: {
-    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-around',
-    marginTop: 20,
-    width: '100%',
   },
-  inputStyle: {
-    backgroundColor: '#fdfdfd',
-    borderColor: '#666',
+  textinput: {
+    backgroundColor: 'lightblue',
+    width: '70%',
+    borderColor: 'black',
     borderWidth: 2,
-    width: '30%',
   },
-  buttonStyle: {
-    fontSize: 28,
-    height: '20%',
+  inputstyle: {
+    marginTop: 10,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
-  buttonView: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  flatliststyle: {
-    flexDirection: 'column',
-    padding: 3,
-  },
-  listStyle: {
-    padding: 3,
+  buttonstyle: {
+    width: '20%',
   },
   listitemstyle: {
-    alignItems: 'center',
-    backgroundColor: '#fdfdfd',
-    borderColor: '#666',
+    backgroundColor: 'lightgreen',
+    width: '90%',
+    borderColor: 'red',
     borderWidth: 2,
+    margin: 5,
   },
-  scrollviewstyle: {
-    flexDirection: 'column',
-    width: '100%',
-    alignSelf: 'center',
-  },
+  scrollviewstyle:{
+    width:'80%',
+  },  
 });
 
 export default App;
-
-const AddBoot = () => {
-  return <Text style={styles.headerStyle}>AddBoot</Text>;
-};
---
+++ task4/components/AddBoot.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import {Text, Modal, View, TextInput, Button, StyleSheet} from 'react-native';
+
+const AddBoot = (props) => {
+    const [id, setId]=useState();
+    const [type, setType]=useState('');
+
+    const idInputHandler=(par)=>{
+        setId(par);
+    }
+    const typeInputHandler=(par)=>{
+        setType(par);
+    }
+    const clearBootData=()=>{
+        setId('');
+        setType('');
+        props.hideInputModal();
+    }
+  return(
+    <Modal visible={props.visibility}>
+      <View style={styles.inputstyle}>
+        <TextInput
+          style={styles.idinput}
+          value={id}
+          onChangeText={idInputHandler}
+          placeholder="Boot id...."
+        />
+        <TextInput
+          style={styles.typeinput}
+          value={type}
+          onChangeText={typeInputHandler}
+          placeholder="Boot type...."
+        />
+      </View>
+      <View style={styles.inputstyle}>
+        <View style={styles.buttonstyle}>
+          <Button title="Cancel" onPress={clearBootData} />
+        </View>
+        <View style={styles.buttonstyle}>
+          <Button title="CLOSE" onPress={()=>props.hideInputModal()} />
+        </View>
+        <View style={styles.buttonstyle}>
+        <Button title="OK" onPress={()=>props.bootDataHandler(id, type)} />
+      </View>
+      </View>        
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    idinput: {
+        backgroundColor: 'lightblue',
+        width: '20%',
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+      typeinput: {
+        backgroundColor: 'lightblue',
+        width: '70%',
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+        inputstyle: {
+      marginTop: 10,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    buttonstyle: {
+      width: '30%',
+    },
+    listitemstyle: {
+      backgroundColor: 'lightgreen',
+      width: '90%',
+      borderColor: 'red',
+      borderWidth: 2,
+      margin: 5,
+    },
+  });
+
+  export default AddBoot;
